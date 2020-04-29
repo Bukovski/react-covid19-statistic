831c450c0fa10f01ece6641e2f9e7a82ecdda0d5
Bukovski <kmanankov@bk.ru>
2020-04-29 11:30:00 +0300

    add: info fast scrolling numbers

--- src/components/info/info.component.jsx
+++ src/components/info/info.component.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import CountUp from 'react-countup';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
-import IsLoading from "../is-loading/is-loading.component";
+import Spinner from "../spinner/spinner.component";
 
 import styles from './info.style.css';
 
@@ -9,68 +10,65 @@ import styles from './info.style.css';
 const Info = (props) => {
 	const { data: { confirmed, recovered, deaths, lastUpdate } } = props;
 	
-	
-	console.log(props.data)
+	if (!confirmed) return <Spinner />;
 	
 	return (
-		<IsLoading isLoading={ !confirmed }>
-			<div className={ styles.container }>
-				<Grid container spacing={ 3 } justify="center">
-					
-					<Grid item component={ Card }>
-						<CardContent>
-							<Typography color="textSecondary" gutterBottom>
-								Infected
-							</Typography>
-							<Typography variant="h5" component="h2">
-								{ confirmed?.value }
-							</Typography>
-							<Typography color="textSecondary">
-								{ new Date(lastUpdate).toDateString() }
-							</Typography>
-							<Typography variant="body2" component="p">
-								Number of active cases of COVID-19.
-							</Typography>
-						</CardContent>
-					</Grid>
-					
-					<Grid item component={ Card }>
-						<CardContent>
-							<Typography color="textSecondary" gutterBottom>
-								Recovered
-							</Typography>
-							<Typography variant="h5" component="h2">
-								{ recovered?.value }
-							</Typography>
-							<Typography color="textSecondary">
-								{ new Date(lastUpdate).toDateString() }
-							</Typography>
-							<Typography variant="body2" component="p">
-								Number of recoveries from COVID-19.
-							</Typography>
-						</CardContent>
-					</Grid>
-					
-					<Grid item component={ Card }>
-						<CardContent>
-							<Typography color="textSecondary" gutterBottom>
-								Deaths
-							</Typography>
-							<Typography variant="h5" component="h2">
-								{ deaths?.value }
-							</Typography>
-							<Typography color="textSecondary">
-								{ new Date(lastUpdate).toDateString() }
-							</Typography>
-							<Typography variant="body2" component="p">
-								Number of deaths caused by COVID-19.
-							</Typography>
-						</CardContent>
-					</Grid>
-					
+		<div className={ styles.container }>
+			<Grid container spacing={ 3 } justify="center">
+				
+				<Grid item component={ Card }>
+					<CardContent>
+						<Typography color="textSecondary" gutterBottom>
+							Infected
+						</Typography>
+						<Typography variant="h5" component="h2">
+							<CountUp start={ 0 } end={ confirmed.value } duration={ 2.75 } separator="," />
+						</Typography>
+						<Typography color="textSecondary">
+							{ new Date(lastUpdate).toDateString() }
+						</Typography>
+						<Typography variant="body2" component="p">
+							Number of active cases of COVID-19.
+						</Typography>
+					</CardContent>
 				</Grid>
-			</div>
-		</IsLoading>
+				
+				<Grid item component={ Card }>
+					<CardContent>
+						<Typography color="textSecondary" gutterBottom>
+							Recovered
+						</Typography>
+						<Typography variant="h5" component="h2">
+							<CountUp start={ 0 } end={ recovered.value } duration={ 2.75 } separator="," />
+						</Typography>
+						<Typography color="textSecondary">
+							{ new Date(lastUpdate).toDateString() }
+						</Typography>
+						<Typography variant="body2" component="p">
+							Number of recoveries from COVID-19.
+						</Typography>
+					</CardContent>
+				</Grid>
+				
+				<Grid item component={ Card }>
+					<CardContent>
+						<Typography color="textSecondary" gutterBottom>
+							Deaths
+						</Typography>
+						<Typography variant="h5" component="h2">
+							<CountUp start={ 0 } end={ deaths.value } duration={ 2.75 } separator="," />
+						</Typography>
+						<Typography color="textSecondary">
+							{ new Date(lastUpdate).toDateString() }
+						</Typography>
+						<Typography variant="body2" component="p">
+							Number of deaths caused by COVID-19.
+						</Typography>
+					</CardContent>
+				</Grid>
+			
+			</Grid>
+		</div>
 	)
 };
 
