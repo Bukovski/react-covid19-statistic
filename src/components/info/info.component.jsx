fdc0f012bf54076f74ef1d8ec816aaf349051ce9
Bukovski <kmanankov@bk.ru>
2020-04-29 10:56:00 +0300

    add: load data from api to info component

--- src/components/info/info.component.jsx
+++ src/components/info/info.component.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
 import IsLoading from "../is-loading/is-loading.component";
 
-import './info.style.css';
+import styles from './info.style.css';
 
 
 const Info = (props) => {
@@ -13,7 +14,62 @@ const Info = (props) => {
 	
 	return (
 		<IsLoading isLoading={ !confirmed }>
-			<h1>Info</h1>
+			<div className={ styles.container }>
+				<Grid container spacing={ 3 } justify="center">
+					
+					<Grid item component={ Card }>
+						<CardContent>
+							<Typography color="textSecondary" gutterBottom>
+								Infected
+							</Typography>
+							<Typography variant="h5" component="h2">
+								{ confirmed?.value }
+							</Typography>
+							<Typography color="textSecondary">
+								{ new Date(lastUpdate).toDateString() }
+							</Typography>
+							<Typography variant="body2" component="p">
+								Number of active cases of COVID-19.
+							</Typography>
+						</CardContent>
+					</Grid>
+					
+					<Grid item component={ Card }>
+						<CardContent>
+							<Typography color="textSecondary" gutterBottom>
+								Recovered
+							</Typography>
+							<Typography variant="h5" component="h2">
+								{ recovered?.value }
+							</Typography>
+							<Typography color="textSecondary">
+								{ new Date(lastUpdate).toDateString() }
+							</Typography>
+							<Typography variant="body2" component="p">
+								Number of recoveries from COVID-19.
+							</Typography>
+						</CardContent>
+					</Grid>
+					
+					<Grid item component={ Card }>
+						<CardContent>
+							<Typography color="textSecondary" gutterBottom>
+								Deaths
+							</Typography>
+							<Typography variant="h5" component="h2">
+								{ deaths?.value }
+							</Typography>
+							<Typography color="textSecondary">
+								{ new Date(lastUpdate).toDateString() }
+							</Typography>
+							<Typography variant="body2" component="p">
+								Number of deaths caused by COVID-19.
+							</Typography>
+						</CardContent>
+					</Grid>
+					
+				</Grid>
+			</div>
 		</IsLoading>
 	)
 };
