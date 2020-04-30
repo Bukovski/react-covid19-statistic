f8be5ee98a86ab0071fed3dc6e1d302e904d473d
Bukovski <kmanankov@bk.ru>
2020-04-30 11:05:00 +0300

    add: chart schedule of sick and dead

--- src/components/chart/chart.component.jsx
+++ src/components/chart/chart.component.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Line, Bar } from 'react-chartjs-2';
 
 import { fetchDailyData } from "../../api";
 
@@ -20,12 +21,37 @@ const Chart = () => {
 	
 	console.log(dailyData)
 	
+	const lineChart = (
+		dailyData[ 0 ]
+			? (<Line
+				data={{
+					labels: dailyData.map(({ date }) => date),
+					datasets: [
+						{
+							data: dailyData.map((data) => data.confirmed),
+							label: 'Infected',
+							borderColor: '#3333ff',
+							fill: true,
+						},
+						{
+							data: dailyData.map((data) => data.deaths),
+							label: 'Deaths',
+							borderColor: 'red',
+							backgroundColor: 'rgba(255,0,0,0.5)',
+							fill: true,
+						}
+					]
+				}}
+			/>)
+			: null
+	);
+	
 	return (
-		<h1>Chart</h1>
-	)
+		<div className="container">
+			{ lineChart }
+		</div>
+	);
 };
 
 
 export default Chart;
-
-// 46.30
