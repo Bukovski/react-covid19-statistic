45fdd8c3d9661e56692f3cedd23fd3aa0f113ffe
Bukovski <kmanankov@bk.ru>
2020-04-30 10:21:14 +0300

    Updates

---
+++ precache-manifest.48560ad438db481caf1de6c154de54f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22b884bbdfe0d58de88a373bd2ba527",
+    "url": "/react-covid19-statistic/index.html"
+  },
+  {
+    "revision": "a709e9a67fb0f53e045b",
+    "url": "/react-covid19-statistic/static/css/main.f5ab3dce.chunk.css"
+  },
+  {
+    "revision": "43cbd31e0e3c4c5439b3",
+    "url": "/react-covid19-statistic/static/js/2.99f44bd8.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/react-covid19-statistic/static/js/2.99f44bd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a709e9a67fb0f53e045b",
+    "url": "/react-covid19-statistic/static/js/main.811da077.chunk.js"
+  },
+  {
+    "revision": "ac665b8f6b61ceca1b0f",
+    "url": "/react-covid19-statistic/static/js/runtime-main.4f5d6ed5.js"
+  }
+]);
